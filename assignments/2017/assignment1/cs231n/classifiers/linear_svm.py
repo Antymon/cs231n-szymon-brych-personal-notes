ccf2ecac60213ab799bf3ceaf08db6a974e30a4c
Szymon Brych <szymon.brych@gmail.com>
2017-06-08 13:44:51 +0800

    [assignment_1][svm] Adding vectorized loss

--- assignments/2017/assignment1/cs231n/classifiers/linear_svm.py
+++ assignments/2017/assignment1/cs231n/classifiers/linear_svm.py
@@ -42,15 +42,20 @@ def svm_loss_naive(W, X, y, reg):
         # !!! added !!!
         # replacing dW row with normalized derivative mentioned in the course notes
         # incorrect class case
-        dW[:,j]+=X[i]/num_train
+        dW[:,j]+=X[i]
     # !!! added !!!
     # replacing dW row with normalized derivative mentioned in the course notes
     # correct class case
-    dW[:, y[i]] += -num_incorrect * X[i]/num_train
+    dW[:, y[i]] += -num_incorrect * X[i]
+
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+
+  # !!! added !!!
+  #also needs to be normalized according to formula
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
@@ -80,18 +85,31 @@ def svm_loss_vectorized(W, X, y, reg):
   dW = np.zeros(W.shape) # initialize the gradient as zero
 
   #############################################################################
-  # TODO:                                                                     #
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+
+  scores = X.dot(W)
+  correct_class_scores = scores[xrange(num_train),y[xrange(num_train)]]
+
+  margins=scores.T - correct_class_scores + 1  # note delta = 1
+  margins[y[xrange(num_train)],xrange(num_train)]=0 #margins are not considered for correct classes
+  margins=np.maximum(0,margins) # discarding satisfied margins
+
+
+  loss = np.sum(margins)
+  loss /= num_train
+  loss += reg * np.sum(W * W)
+
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
 
 
   #############################################################################
-  # TODO:                                                                     #
   # Implement a vectorized version of the gradient for the structured SVM     #
   # loss, storing the result in dW.                                           #
   #                                                                           #
@@ -99,7 +117,30 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-  pass
+
+  #margins were clamped to 0 from bottom before
+  #first clamp margin values to 1 from top
+  #then all positive values will be ceiled to 1
+  #giving identity coefs for incorrect class gradients and zeroing all other cases
+  margins_gradient_coefficients=np.ceil(np.minimum(1,margins))
+
+  #1s indicate incorrect margin penalties so we sum them up for each example
+  sums = np.sum(margins_gradient_coefficients, axis=0)
+
+  #for each correct class in each example alter value from 0 to -sums
+  #which is the correct class' gradient coefficient
+  margins_gradient_coefficients[y[xrange(num_train)], xrange(num_train)] = -sums
+
+  dW = dW.T
+  #perform following multiplication to formulate correct gradient for all cases
+  #respecting both gradients introduced in the course notes
+  dW[xrange(num_classes)] += margins_gradient_coefficients.dot(X)
+
+  dW=dW.T
+  dW/=num_train
+  dW+= 2* reg * W
+
+
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
